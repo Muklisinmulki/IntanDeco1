25f9158e3b23ae61b0300c96044bfdcb0bbb3ccf
muklisin mulki <muklisinm29@gmail.com>
2025-04-28 12:58:02 +0700

    update perubahan coding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,15 @@
 import '../css/app.css';
 import './bootstrap';
 import 'flowbite';
+
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Ziggy } from 'ziggy-js';
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
